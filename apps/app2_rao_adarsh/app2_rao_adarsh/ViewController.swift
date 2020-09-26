e543dc3769667b4d4d6801331b7abd08a069ed6c
Adarsh Rao <raoadarsh123@gmail.com>
2020-09-25 22:18:16 -0400

    part 2 of app 2

--- apps/app2_rao_adarsh/app2_rao_adarsh/ViewController.swift
+++ apps/app2_rao_adarsh/app2_rao_adarsh/ViewController.swift
@@ -25,6 +25,8 @@ class ViewController: UIViewController {
     @IBOutlet weak var mainTitle: UILabel!
     
     @IBOutlet weak var p1Score: UILabel!
+    var one = 0;
+    var two = 0;
     
     @IBOutlet weak var p2Score: UILabel!
     
@@ -32,35 +34,116 @@ class ViewController: UIViewController {
     
     @IBOutlet lazy var collectionOfButtons: Array<UIButton> = [UIButton]();
     
+    var winningCombos: [[Int]] = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [2, 4, 6], [0, 4, 8]]
+    
+    var gameTracker: [Int] = [0,0,0,0,0,0,0,0,0];
+    
+    var p1: Bool = false;
+    var p2: Bool = false;
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        p1 = true;
+        gameState.text = "Player 1's Turn";
         
+        p1Score.text = p1Score.text! + String(one);
+        p2Score.text = p2Score.text! + String(two);
     }
     
 
 
     @IBAction func btnClick(_ sender: UIButton) {
-        let image = Bool.random();
-        if image {
-            sender.setImage(UIImage(named: "mark-x"), for: .normal);
-            mainTitle.text = "Jerry";
-            p1Score.text = "Ye";
-            p2Score.text = "Is";
-            gameState.text = "Awesome";
-        } else {
-            sender.setImage(UIImage(named: "mark-o"), for: .normal);
-            mainTitle.text = "Adarsh";
-            p1Score.text = "Rao";
-            p2Score.text = "Takes";
-            gameState.text = "CIS 195";
-        }
-        print(sender.tag);
+        if p1 {
+            if sender.isEnabled == true {
+                let buttonImage = UIImage(named: "mark-x")?.withRenderingMode(.alwaysTemplate)
+                sender.setImage(buttonImage, for: .normal);
+                p1 = false;
+                p2 = true;
+                gameState.text = "Player 2's Turn";
+                sender.isEnabled = false;
+                gameTracker[sender.tag - 1] = 1;
+            } else {
+                print("Send an alert")
+            }
             
+        } else {
+            if sender.isEnabled == true {
+                let buttonImage = UIImage(named: "mark-o")?.withRenderingMode(.alwaysTemplate)
+                sender.setImage(buttonImage, for: .normal);
+                gameState.text = "Player 1's Turn";
+                p1 = true;
+                p2 = false;
+                sender.isEnabled = false;
+                gameTracker[sender.tag - 1] = -1;
+            } else {
+                print("send an alert");
+            }
+            
+        }
+        var draw: Bool = true;
+        
+        for b in collectionOfButtons {
+            if b.isEnabled {
+                draw = false
+            }
+        }
+        if draw {
+            gameState.text = "Draw!";
+            gameEnd();
+            for b in collectionOfButtons {
+                b.tintColor = .systemGray;
+            }
+        }
+        else {
+            for combo in winningCombos {
+                if gameTracker[combo[0]] != 0 && gameTracker[combo[0]] == gameTracker[combo[1]] && gameTracker[combo[1]] == gameTracker[combo[2]] {
+                    for i in combo {
+                        for b in collectionOfButtons {
+                            if b.tag - 1 == i {
+                                b.tintColor = .systemYellow;
+                            }
+                            b.isEnabled = false;
+                        }
+                    }
+                    gameState.textColor = UIColor.green;
+                    if gameTracker[combo[0]] == -1 {
+                        gameState.text = "Player 2 Wins";
+                        two+=1;
+                    } else {
+                        gameState.text = "Player 1 Wins";
+                        one+=1;
+                    }
+                    p1Score.text = "Player 1 Score: " + String(one);
+                    p2Score.text = "Player 2 Score: " + String(two);
+                    gameEnd();
+                }
+            }
+        }
+        
     }
     
     @IBAction func clearBoard(_ sender: UIButton) {
+        gameState.textColor = UIColor.black;
+        gameState.text = "Player 1's Turn";
+        p1 = true;
+        p2 = false;
         for b in collectionOfButtons {
-             b.setImage(UIImage(named: "mark-none"), for: .normal);
+            let buttonImage = UIImage(named: "mark-none")?.withRenderingMode(.alwaysTemplate)
+             b.setImage(buttonImage, for: .normal);
+             b.isEnabled = true;
+             b.tintColor = .systemBlue;
+        }
+        for i in 0..<gameTracker.count {
+            gameTracker[i] = 0;
+        }
+    }
+    func gameEnd() {
+        for b in collectionOfButtons {
+             b.isEnabled = false;
+        }
+        for i in 0..<gameTracker.count {
+            gameTracker[i] = 0;
         }
     }
 }
